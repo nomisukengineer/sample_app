46833d48642d69bf04576b0c2ffcc3562017e6e3
nomi <nomi.naohiro@lmi.ne.jp>
2019-10-09 03:00:02 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 #  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -76,6 +85,38 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+    # パスワード再設定の期限が切れている場合はtrueを返す
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+    end
+  
+    # ユーザーのステータスフィードを返す
+    def feed
+      Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+      following_ids: following_ids, user_id: id)
+    end
+  
+    # ユーザーをフォローする
+    def follow(other_user)
+      following << other_user
+    end
+
 
   private
 
